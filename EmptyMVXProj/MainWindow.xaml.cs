20469c5fad15a014d8138ab0670606dbbf353dba
Andrew Rae <andrew.rae@inps.co.uk>
2017-01-31 14:35:27 +0000

    Added in mahApps for testing fullscreen

--- EmptyMVXProj/MainWindow.xaml.cs
+++ EmptyMVXProj/MainWindow.xaml.cs
@@ -12,17 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace EmptyMVXProj
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    public partial class MainWindow : MetroWindow
+    { 
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        /// <summary>
+        /// Called when [loaded].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="routedEventArgs">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            WindowState = WindowState.Maximized;
+            ShowMinButton = true;
+            Loaded -= OnLoaded;
         }
     }
 }
