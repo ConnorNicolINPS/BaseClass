f8a6431e244670127552a6040ab1851c9520d213
Andrew Rae <andrew.rae@inps.co.uk>
2017-02-01 10:00:31 +0000

    Dockable window set.

--- EmptyMVXProjCore/ViewModels/MainWindowViewModel.cs
+++ EmptyMVXProjCore/ViewModels/MainWindowViewModel.cs
@@ -6,17 +6,34 @@
     {
         private bool hideTaskBar;
 
+        private bool isDraggable;
+
+        private string isMaximized;
+
         private MvxCommand hideButtonCommand;
 
         public MainWindowViewModel()
         { 
             this.HideTaskBar = true;
+            this.IsDraggable = false;
+            this.IsMaximized = "Maximized";
         }
 
         public bool HideTaskBar 
         {
             get { return this.hideTaskBar; }
             set { this.SetProperty(ref this.hideTaskBar, value); }
+        }
+        public bool IsDraggable
+        {
+            get { return this.isDraggable; }
+            set { this.SetProperty(ref this.isDraggable, value); }
+        }
+
+        public string IsMaximized
+        {
+            get { return this.isMaximized; }
+            set { this.SetProperty(ref this.isMaximized, value); }
         }
 
         public MvxCommand HideButtonCommand
@@ -30,6 +47,16 @@
         private void HideButtonAction()
         {
             HideTaskBar = !HideTaskBar;
+            IsDraggable = !IsDraggable;
+
+            if (HideTaskBar)
+            {
+                IsMaximized = "Maximized";
+            }
+            else
+            {
+                IsMaximized = "Normal";
+            }
         }
     }
 }
