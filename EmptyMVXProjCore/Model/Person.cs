2700537b7c1877bd9603f417a21d106b65ad2ffd
INPS\Connor.Nicol <Connor. Nicol@INPS.co.uk>
2017-06-01 11:36:37 +0100

    added person model and started logic

---
+++ EmptyMVXProjCore/Model/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace EmptyMVXProjCore.Model
+{
+    public class Person
+    {
+        private string title;
+        private string forename;
+        private string surname;
+
+        public Person(string title, string forename, string surname)
+        {
+            this.Title = title;
+            this.Forename = forename;
+            this.Surname = surname;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Forename
+        {
+            get { return forename; }
+            set { forename = value; }
+        }
+        
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+    }
+}
--- EmptyMVXProjCore/ViewModels/FirstViewModel.cs
+++ EmptyMVXProjCore/ViewModels/FirstViewModel.cs
@@ -1,9 +1,73 @@
 using MvvmCross.Core.ViewModels;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System;
+using EmptyMVXProjCore.Model;
 
 namespace EmptyMVXProjCore.ViewModels
 {
     public class FirstViewModel : MvxViewModel
     {
-        private MvxCommand 
+        private ICommand getNamesCommand;
+        private ICommand printNamesCommamd;
+        private ObservableCollection<Person> listOfNames;
+
+        public FirstViewModel()
+        {
+            this.ListOfNames = new ObservableCollection<string>();
+        }
+
+        public ICommand GetNamesCommand
+        {
+            get { return this.getNamesCommand ?? (this.getNamesCommand = new MvxCommand(GetNamesAction)); }
+        }
+
+        public ICommand PrintNamesCommamd
+        {
+            get { return this.printNamesCommamd ?? (this.printNamesCommamd = new MvxCommand(PrintNamesAction)); }
+        }
+
+        public ObservableCollection<Person> ListOfNames
+        {
+            get { return this.listOfNames; }
+            set { this.SetProperty(ref this.listOfNames, value); }
+        }
+
+
+        private void GetNamesAction()
+        {
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Ronald", "Weasly"));
+            ListOfNames.Add(new Person("Miss", "Hermiony", "Granger"));
+            ListOfNames.Add(new Person("Miss", "Ginny", "Weasly"));
+            ListOfNames.Add(new Person("Mr", "Fred", "Weasly"));
+            ListOfNames.Add(new Person("Mr", "George", "Weasly"));
+            ListOfNames.Add(new Person("Mr", "Percy", "Weasly"));
+            ListOfNames.Add(new Person("Mr", "Charles", "Weasly"));
+            ListOfNames.Add(new Person("Mr", "James", "Potter"));
+            ListOfNames.Add(new Person("Mrs", "Lilly", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Sirious", "Black"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+            ListOfNames.Add(new Person("Mr", "Harry", "Potter"));
+
+        }
+
+        private void PrintNamesAction()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
