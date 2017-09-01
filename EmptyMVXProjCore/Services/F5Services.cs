dacb8ced0aa343b6e50305f5dda07b3d3a9d195a
INPS\Connor.Nicol <Connor. Nicol@INPS.co.uk>
2017-09-01 08:50:09 +0100

    made a start to the service

---
+++ EmptyMVXProjCore/Services/F5Services.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyMVXProjCore.Services
+{
+    class F5Services
+    {
+        private const string BASEURL = "http://localhost:8080";
+
+        HttpResponseMessage Response;
+        HttpClient Client;
+
+        public F5Services()
+        {
+            this.Response = new HttpResponseMessage();
+            this.Client = new HttpClient();
+            Client.BaseAddress = new Uri(BASEURL);
+        }
+
+        public async  Task<string> GetF5Response()
+        {
+
+            return "";
+        }
+    } 
+}
+
+
